11cd8255e61280432e4a2ec263b5e4a4983b412a
Sylvain <webmaster@ecortex.fr>
2016-08-10 22:09:47 +0200

    ajoute balises meta opengraph  / ajoute favicons

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -7,7 +7,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="">
     <meta name="author" content="CODEheures | Sylvain Gagnot">
-    <link rel="icon" type="image/png" href="{{ asset('css/images/favicon.png') }}">
+    <meta property="og:site_name" content="CODEheures"/>
+    <meta property="og:title" content="Développeur WEB indépendant"/>
+    <meta property="og:description" content="CODEheures propose une solution innovante: les Devis et Tarifs BackTime. Vos heures non consommées seront reportées."/>
+    <meta property="og:image" content="{{ asset('css/images/codeheures_thumbnail.jpg') }}">
+    <meta property="og:url" content="{{ route('home') }}">
+    <meta property="og:type" content="website"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#da713b">
+    <meta name="theme-color" content="#ffffff">
     <title>@yield('title')</title>
     @yield('css')
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,800,400,600|Arvo:700|Flamenco:300' rel='stylesheet' type='text/css'>
